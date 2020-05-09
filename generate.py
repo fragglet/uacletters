840943f2344674abba9d9d21b0e11a996dc144ff
Simon Howard <fraggle@soulsphere.org>
2020-05-09 17:15:15 -0400

    Add WAD generation

--- generate.py
+++ generate.py
@@ -3,6 +3,75 @@
 from PIL import Image
 from glob import glob
 import os
+import omg
+
+OUTPUT_WORDS = """
+U W U
+A
+Acacia
+ai
+al
+Alicia
+all
+allow
+aw
+awl
+Awol
+Caca
+cacao
+calcic
+calculi
+calico
+call
+caw
+cilia
+claw
+Clio
+cloaca
+coal
+coca
+cola
+cocoa
+coil
+colic
+cow
+cowl
+Cuculi
+cull
+I
+ill
+Io
+Iowa
+jail
+jaw
+Jill
+Jo
+Julia
+julio
+law
+Lila
+lilac
+lill
+local
+Lou
+Lucia
+Lucilia
+Lula
+lull
+Lulu
+Ouija
+owl
+Waco
+wail
+wall
+Wallawalla
+wallow
+Will
+willow
+woo
+wool
+wow
+"""
 
 def columns_from_image(filename):
     img = Image.open(filename).convert('RGB')
@@ -93,8 +162,67 @@ def read_letters():
 
     return result
 
-letters = read_letters()
-for name, ranges in letters.items():
-    print("%r: %r" % (name, ranges))
+def make_adder(collection, struct_type):
+    def adder(**kwargs):
+        result = len(collection)
+        collection.append(struct_type(**kwargs))
+        return result
+    return adder
 
+def make_phrase(s, letters):
+    s = s.lower()
+    result = []
+    for idx, c in enumerate(s):
+        if c == ' ':
+            letter = letters['long_space']
+        else:
+            letter = letters[c]
+        result.extend(letter)
+        if idx + 1 < len(s) and s[idx + 1] != ' ':
+            result.extend(letters['short_space'])
+    return result
+
+letters = read_letters()
+
+ed = omg.MapEditor()
+
+add_sector = make_adder(ed.sectors, omg.Sector)
+add_sidedef = make_adder(ed.sidedefs, omg.Sidedef)
+add_linedef = make_adder(ed.linedefs, omg.Linedef)
+add_vertex = make_adder(ed.vertexes, omg.Vertex)
+add_thing = make_adder(ed.things, omg.Thing)
+
+sec1 = add_sector(z_ceil=96)
+sd1 = add_sidedef(sector=sec1, tx_mid="STARTAN2")
+v1 = add_vertex(x=256, y=8192)
+v2 = add_vertex(x=256, y=0)
+v3 = add_vertex(x=0, y=0)
+v4 = add_vertex(x=0, y=8192)
+
+# Three boring walls
+add_linedef(vx_a=v4, vx_b=v1, flags=1, front=sd1)
+add_linedef(vx_a=v1, vx_b=v2, flags=1, front=sd1)
+add_linedef(vx_a=v2, vx_b=v3, flags=1, front=sd1)
+
+phrase = " ".join(OUTPUT_WORDS.strip().split("\n"))
+
+# The fourth wall is built out of small segments:
+last_v = v3
+y = 0
+for offset, npixels in make_phrase(phrase, letters):
+    sd = add_sidedef(sector=sec1, tx_mid="SHAWN1", off_x=offset)
+    v = add_vertex(x=0, y=y+npixels)
+    add_linedef(vx_a=last_v, vx_b=v, flags=1, front=sd)
+    last_v = v
+    y += npixels
+
+# Tailing end to join back with first linedef
+add_linedef(vx_a=last_v, vx_b=v4, front=sd1, flags=1)
+
+# Player 1 start
+add_thing(x=128, y=128, type=1)
+
+w = omg.WAD()
+w.maps["MAP01"] = ed.to_lumps()
+w.to_file("foo.wad")
 
